31a161b2bbd3332489a5e4b6c8e4416bdc366a6d
shreenidhi8095 <shreenidhi.an@gmail.com>
2020-10-08 23:53:37 +0530

    commit table3

---
+++ src/Sm/Table3.java
@@ -0,0 +1,14 @@
+package Sm;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Table3 {
+@Test
+public void t1()
+{
+	int a=3;
+	int b=6;
+System.out.println(a+b);
+}
+}
